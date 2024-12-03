90ef7d5bc2b63bd70ee792445724781018267357
Hippophile <paschalakisfilip@gmail.com>
2024-12-03 19:43:10 +0200

    very happy for lab6.1.cpp !!!

--- Ksergias/lab6.1.cpp
+++ Ksergias/lab6.1.cpp
@@ -14,3 +14,62 @@ virtual).*/
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Machinery {
+private:
+    string name;
+    double weight;
+
+public:
+    Machinery(const string& name, double w) : name(name), weight(w) {}
+
+    virtual ~Machinery(){}
+
+    string get_name () const {
+        return name;
+    }   
+
+    double get_weight () const {
+        return weight;
+    }
+
+    virtual void operate (int hours = 1 ) const {
+        cout << "Machinery worked for " << hours << " hours" << endl;
+    }
+
+
+};
+
+class Vehicle : public Machinery {
+public:
+    Vehicle(const string& name, double w) : Machinery(name, w) {};
+
+    void operate(int hours = 1) const override {
+        cout << "Vehicle worked for " << hours << " hours" << endl;
+    }
+
+};
+
+class Computer : public Machinery {
+public:
+    Computer(const string& name, double w) : Machinery(name, w) {};
+
+    void operate(int hours = 1) const override {
+        // Machinery::operate(hours);
+        cout << "Computer was in use for " << hours << " hours" << endl;
+    }
+
+
+};
+
+int main() {
+    Vehicle v("Car", 1200.0);         // Δημιουργία αντικειμένου Vehicle
+    Computer c("Laptop", 2.5);       // Δημιουργία αντικειμένου Computer
+
+    v.operate(4);                    // Vehicle worked for 4 hours
+    c.operate(2);                    // Computer was in use for 2 hours
+
+    return 0;
+}
+
+
